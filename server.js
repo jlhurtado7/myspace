1e556b048bbfe9efb3e5304b7b2683e542fb4886
jl hurtado <j.holivares@outlook.com>
2025-05-12 09:52:46 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
